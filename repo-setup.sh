039afe12c7bb4832c0e3a3abd8a4a4768bd16a87
jakengatchu <jngatchu@gmail.com>
2023-12-06 11:15:08 +0000

    Add invoicing solution submodule

--- repo-setup.sh
+++ repo-setup.sh
@@ -10,8 +10,10 @@ add_submodule() {
   git submodule add "$repo_url" "$subdirectory/$submodule_path"
 }
 
+encoded_password=$(printf %s '57tXr#@2Z8hC$6sG^sRms' | jq -s -R -r @uri)
+
 # Add Submodules with Subdirectories
-add_submodule "https://greatgatchby:57tXr#@2Z8hC$6sG^sRms@github.com/greatgatchby/circuitai-invoice-processor.git" "invoice-processor" "solutions"
+add_submodule "https://greatgatchby:${encoded_password}@github.com/greatgatchby/circuitai-invoice-processor.git" "invoice-processor" "solutions"
 add_submodule "https://github.com/coqui-ai/TTS.git" "TTS" "ai-services"
 add_submodule "https://github.com/openai/whisper.git" "whisper" "ai-services"
 add_submodule "https://github.com/Stability-AI/generative-models.git" "stability" "ai-services"
